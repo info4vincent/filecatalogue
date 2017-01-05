60c9b022b93eb28014c5e68542b63efa1d7815e0
info4vincent <info4vincent@example.com>
2017-01-05 06:32:45 +0000

    Added mongodb, with backuplocation based on sha1

--- totalfiles/main.go
+++ totalfiles/main.go
@@ -2,19 +2,87 @@ package main
 
 import (
 	"crypto/sha1"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"math"
+	"net"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // 8KB
 const filechunk = 8192
 
-func TraverseDir(dirName string) {
+type CollectionInfo struct {
+	DBName           string
+	FullRootName     string
+	CollectionName   string
+	BackupCollection string
+}
+
+// Collection CollectionMetaData of disk
+var Collection = CollectionInfo{DBName: "filecatalogue", FullRootName: "e:/Testme", CollectionName: "TestMe", BackupCollection: "TestMe2"}
+
+// NodeInfo a file node meta information
+type NodeInfo struct {
+	FullName       string
+	Name           string
+	FileSize       int64
+	Sha1           string
+	BackupLocation string
+}
+
+func writeToDb(session *mgo.Session, nodeInfo NodeInfo) {
+	c := session.DB(Collection.DBName).C(Collection.CollectionName)
+
+	c2 := session.DB(Collection.DBName).C(Collection.BackupCollection)
+
+	var result NodeInfo
+	err2 := c2.Find(bson.M{"sha1": nodeInfo.Sha1}).One(&result)
+
+	if err2 == nil || err2.Error() != "not found" {
+		nodeInfo.BackupLocation = fmt.Sprintf("%s@%s", Collection.BackupCollection, result.FullName)
+	} else if err2 != nil && err2.Error() == "not found" {
+		nodeInfo.BackupLocation = ""
+	} else {
+		log.Fatal(err2)
+	}
+
+	_, err := c.UpsertId(nodeInfo.FullName, &nodeInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// func findDuplicates(session *mgo.Session) {
+// 	c1 := session.DB(Collection.DBName).C(Collection.CollectionName)
+
+// 	var results []NodeInfo
+
+// 	err := c1.Find(nil).All(&results)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	result := NodeInfo{}
+// 	for _, r := range results {
+// 		err = c2.Find(bson.M{"sha1": r.Sha1}).One(&result)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		result.BackupLocation = fmt.Sprintf("%s::%s", Collection.BackupCollection, r.FullName)
+// 	}
+// }
+
+// TraverseDir Search in dir and searches for files.
+func TraverseDir(session *mgo.Session, dirName string) {
 	files, err := ioutil.ReadDir(dirName)
 	if err != nil {
 		log.Fatal(err)
@@ -25,14 +93,14 @@ func TraverseDir(dirName string) {
 		fullFile, _ := filepath.Abs(newDir)
 		fmt.Println(fullFile)
 		if file.IsDir() {
-			TraverseDir(fullFile)
+			TraverseDir(session, fullFile)
 		} else {
-			calcSHA1(fullFile)
+			calcSHA1(session, fullFile)
 		}
 	}
 }
 
-func calcSHA1(aFile string) {
+func calcSHA1(session *mgo.Session, aFile string) {
 
 	// Open the file for reading
 	file, err := os.Open(aFile)
@@ -77,8 +145,69 @@ func calcSHA1(aFile string) {
 
 	// Output the results
 	fmt.Printf("%x\n", hash.Sum(nil))
+
+	fullfileName := strings.Replace(aFile, "\\", "/", -1)
+	nodeInfo := NodeInfo{FullName: fullfileName, Name: info.Name(), FileSize: filesize}
+	nodeInfo.Sha1 = fmt.Sprintf("%x", hash.Sum(nil))
+
+	writeToDb(session, nodeInfo)
+}
+
+func connectDB() *mgo.Session {
+	//uri := os.Getenv("MONGODB_URL")
+	uri := "mongodb://mongodb1584:YXo7XJZu4uT22axJ0ceECSgsggh4SpwFDhKHU6wy2nIW28gEwMqi78UEEo9cbfY265y9ABoYLC2nWdEV2JvjqQ==@mongodb1584.documents.azure.com:10250"
+	if uri == "" {
+		fmt.Println("No connection string provided - set MONGODB_URL")
+		os.Exit(1)
+	}
+	uri = strings.TrimSuffix(uri, "?ssl=true")
+
+	tlsConfig := &tls.Config{}
+	tlsConfig.InsecureSkipVerify = true
+
+	dialInfo, err := mgo.ParseURL(uri)
+
+	if err != nil {
+		fmt.Println("Failed to parse URI: ", err)
+		os.Exit(1)
+	}
+
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+		return conn, err
+	}
+
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		fmt.Println("Failed to connect: ", err)
+		os.Exit(1)
+	}
+
+	// c := session.DB("test").C("people")
+	// err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+	// 	&Person{"Cla", "+55 53 8402 8510"})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	dbnames, err := session.DB("").CollectionNames()
+	if err != nil {
+		fmt.Println("Couldn't query for collections names: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(dbnames)
+
+	return session
 }
 
 func main() {
-	TraverseDir("e:/GoogleDrive")
+	session := connectDB()
+	defer session.Close()
+	//initDB()
+	TraverseDir(session, Collection.FullRootName)
+
+	Collection = CollectionInfo{DBName: "filecatalogue", FullRootName: "e:/Testme2", CollectionName: "TestMe2", BackupCollection: "TestMe"}
+
+	TraverseDir(session, Collection.FullRootName)
 }
